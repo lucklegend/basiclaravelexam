57d775915a65a50c2be4acad51cf6573375bc009
Janno E. Nuyles <lucklegend@gmail.com>
2022-03-02 08:48:42 +0800

    configuration for heroku

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL = parse_url('DATABASE_URL');
+
 return [
 
     /*
@@ -52,8 +54,8 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            'charset' => 'utf8', // utf8mb4
+            'collation' => 'utf8_unicode_ci', //utf8mb4_unicode_ci
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,6 +18,44 @@ class UserSeeder extends Seeder
     public function run()
     {
         //
+        DB::table('users')->insert([
+            'name' => 'Master',
+            'sponsor_id' => 0,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'John',
+            'sponsor_id' => 1,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Mary',
+            'sponsor_id' => 1,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Paul',
+            'sponsor_id' => 2,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Nick',
+            'sponsor_id' => 3,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Anne',
+            'sponsor_id' => 5,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        
+
         for($i=1; $i<10;$i++){
             DB::table('users')->insert([
                 'name' => Str::random(10),
